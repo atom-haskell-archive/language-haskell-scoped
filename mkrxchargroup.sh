ac8392eba35eb49a72ed6d8a224c1faed5a25ef5
Nikolay Yakimov <root@livid.pp.ru>
2017-07-24 21:02:07 +0300

    Regexp generator rewrite

--- mkrxchargroup.sh
+++ mkrxchargroup.sh
@@ -1,13 +1,28 @@
+PRINTFMT='\\u{%X}'
+FLAGS='u'
+if [[ "$1" == '-a' ]]; then
+  ASCII='val < 128 && '
+  PRINTFMT='\\%c'
+  FLAGS=''
+  shift
+fi
 gawk -F\; '{if($3~"^[SP]") print $1;}' /usr/share/unicode-data/UnicodeData.txt \
 | awk \
-  'BEGIN {prev=0; start=0;};
-  {
+  '{
     val=strtonum("0x"$0);
-    if (val-prev>1) {
-      print sprintf("\\u{%x}-\\u{%x}",start,prev);
-      start=val;
+    if (NR == 1) { prev=val; start=val; }
+    char = sprintf("%c", val);
+    if ('"$ASCII"' char!~/[(),;\[\]`\{\}_"'"'"']/) {
+      if (val-prev>1) {
+	if (start == prev) {
+          print sprintf("'"$PRINTFMT"'",start);
+	} else {
+          print sprintf("'"$PRINTFMT"'-'"$PRINTFMT"'",start,prev);
+	}
+        start=val;
+      }
+      prev=val
     }
-    prev=val
   }' \
-| tail -n+2 | tr -d '\n' \
-| sed -r 's~^(.*)$~module.exports=/(?:(?![(),;\\[\\]`\\{\\}_"'"'"'])[\1])+/u'
+| tr -d '\n' \
+| sed -r 's~^(.*)$~/'$1'[\1]+'$2'/'"${FLAGS}$3"'\n~'
