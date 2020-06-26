bddce08a664ed2541ad0fbdaaebc95b63753fde9
Nikolay Yakimov <root@livid.pp.ru>
2020-06-26 16:14:22 +0300

    Update dependencies

--- src/language-haskell-scoped.ts
+++ src/language-haskell-scoped.ts
@@ -3,12 +3,13 @@ import { EditorController } from './editor-controller'
 import { ICompletionBackend } from 'atom-haskell-upi/completion-backend'
 
 let resolveCB: ((val: ICompletionBackend) => void) | undefined
-let cbPromise: Promise<ICompletionBackend> | undefined
 let disposables: Atom.CompositeDisposable | undefined
 
 export function activate() {
   resolveCB = undefined
-  const bprom = (cbPromise = new Promise((resolve) => (resolveCB = resolve)))
+  const bprom = new Promise<ICompletionBackend>(
+    (resolve) => (resolveCB = resolve),
+  )
   const bdisp = (disposables = new Atom.CompositeDisposable())
   disposables.add(
     atom.workspace.observeTextEditors(async (ed) => {
@@ -21,7 +22,6 @@ export function activate() {
 
 export function deactivate() {
   resolveCB = undefined
-  cbPromise = undefined
   disposables && disposables.dispose()
   disposables = undefined
 }
